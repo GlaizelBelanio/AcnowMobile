014d2f7a012e9983daaed7b97b0bd47286d1224b
Glaizel Nicole Belanio <124427155+GlaizelBelanio@users.noreply.github.com>
2024-10-05 16:34:00 +0800

    Initial Commit

---
+++ AcNow MobileTests/AcNow_MobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  AcNow_MobileTests.swift
+//  AcNow MobileTests
+//
+//  Created by Glaizel Belanio on 10/5/24.
+//
+
+import Testing
+@testable import AcNow_Mobile
+
+struct AcNow_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
